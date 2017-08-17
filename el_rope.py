e9f25f756863f5b04855d5a34aabfa780094fdf3
Todd Dembrey <todd.dembrey@gmail.com>
2017-08-17 22:28:44 +0100

    Return the changes to the current browser

--- el_rope.py
+++ el_rope.py
@@ -1,7 +1,19 @@
 import argparse
 
 from rope.base import libutils, project
+from rope.base.fscommands import FileSystemCommands
 from rope.refactor.extract import ExtractVariable
+
+
+class EmacsSystemCommands(FileSystemCommands):
+    def __init__(self, current_path):
+        self.current_path = current_path
+
+    def write(self, path, data):
+        if path == self.current_path:
+            print(data)
+        else:
+            super(EmacsSystemCommands, self).write(path, data)
 
 
 parser = argparse.ArgumentParser()
@@ -13,10 +25,13 @@ parser.add_argument('name', type=str)
 
 args = parser.parse_args()
 
-project_path = args.path
-base_project = project.Project(project_path)
-
 file_path = args.file
+
+file_system_commands = EmacsSystemCommands(file_path)
+
+project_path = args.path
+base_project = project.Project(project_path, fscommands=file_system_commands)
+
 resource = libutils.path_to_resource(base_project, file_path)
 
 start, end = args.start - 1, args.end
@@ -25,5 +40,6 @@ extractor = ExtractVariable(base_project, resource, start, end)
 variable_name = args.name
 changes = extractor.get_changes(variable_name, similar=True)
 
-print(changes.get_description())
-# base_project.do(changes)
+base_project.do(changes)
+
+base_project.close()
